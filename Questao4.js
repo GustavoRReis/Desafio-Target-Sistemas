784474d6fb8c3a5f4dd9dfa21ddfc4cbf31e12db
gustavo ramon dos reis <gustavobilireis@hotmail.com>
2023-03-10 18:55:56 -0300

    Questão4

---
+++ Questao4.js
@@ -0,0 +1,7 @@
+/* Dois veículos (um carro e um caminhão) saem respectivamente de cidades opostas pela mesma rodovia. 
+O carro de Ribeirão Preto em direção a Franca, a uma velocidade constante de 110 km/h e o caminhão de Franca 
+em direção a Ribeirão Preto a uma velocidade constante de 80 km/h. Quando eles se cruzarem na rodovia, 
+qual estará mais próximo a cidade de Ribeirão Preto? */
+
+//A distancia dos dois da cidade de Ribeirão Preto é a mesma no ponto que se cruzam, pois estao exatamente no memso lugar
+//Poderiamos entrar no debate de quem andou mais, tempo x distancia... porem o ponto de encontro é igual para ambos.
